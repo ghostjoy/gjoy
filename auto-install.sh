36228ca28daf0120ff1a0bca351e2034ba545792
曾天威 <ztw@ztws-mbpr15.local>
2016-01-16 22:35:59 +0800

    ok

--- auto-install.sh
+++
@@ -1,1 +0,0 @@
-git clone git://github.com/ghostjoy/gjoy.git ~/gjoy
---
+++ download.sh
@@ -0,0 +1,3 @@
+#https://gist.github.com/kevin-smets/8568070
+git clone git://github.com/ghostjoy/gjoy.git ~/gjoy
+git clone https://github.com/powerline/fonts ~/gjoy/powerlineFonts
