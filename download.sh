ae53f5f1ac52fa6ad945d2070beb54e69bb742fc
曾天威 <ztw@jedi.local>
2016-01-18 00:40:41 +0800

    remark download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #https://gist.github.com/kevin-smets/8568070
 #brew install zsh-syntax-highlighting
-git clone git://github.com/ghostjoy/gjoy.git ~/gjoy
-git clone https://github.com/powerline/fonts ~/gjoy/powerlineFonts
+#git clone git://github.com/ghostjoy/gjoy.git ~/gjoy
+#git clone https://github.com/powerline/fonts ~/gjoy/powerlineFonts
